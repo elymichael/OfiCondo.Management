ac8d338b0e321b08c8b3b14467467b74309d0eb3
Ely Michael Núñez <elymichael@gmail.com>
2021-08-16 18:14:22 -0400

    Upload project web page.

---
+++ Oficondo.Management.Web.App/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Oficondo.Management.Web.App.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
